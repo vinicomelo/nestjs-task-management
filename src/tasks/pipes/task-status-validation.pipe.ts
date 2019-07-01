c7fa1c3711547fc3c4775e7adcd8301aad7f1ee4
Vico Melo <vinicomelo@gmail.com>
2019-06-30 20:54:23 -0400

    clean

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -1,4 +1,4 @@
-import {  BadRequestException, PipeTransform } from '@nestjs/common';
+import { BadRequestException, PipeTransform } from '@nestjs/common';
 import { TaskStatus } from '../task.model';
 
 export class TaskStatusValidationPipe implements PipeTransform {
